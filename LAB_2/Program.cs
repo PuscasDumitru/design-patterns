0634d0130e7b679f28f5ad55e30bfeb40dc0c32c
PuscasDumitru <71260186+PuscasDumitru@users.noreply.github.com>
2021-11-13 20:04:19 +0200

    Add files via upload

---
+++ LAB_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VehicleBuilder builder;
+            Document doc;
+            Shop shop = new Shop();
+            RandomNonBuyers dontBuy = RandomNonBuyers.GetRandomNonBuyers();
+
+            for (int i = 0; i < 15; i++)
+            {
+                string nbuy = dontBuy.NonBuyer;
+                Console.WriteLine(nbuy);
+            }
+
+            Console.WriteLine("\n\n");
+
+            builder = new ScooterBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+            doc = new PaymentCheck();
+            doc.ListPages();
+
+            builder = new CarBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+            doc = new PaymentCheck();
+            doc.ListPages();
+
+            builder = new MotorCycleBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+            doc = new Credit();
+            doc.ListPages();
+
+        }
+    }
+}
